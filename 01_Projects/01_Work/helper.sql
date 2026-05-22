208f996dbdccde7548a16a89b090f1085c0d8b57
Brandon J Knight <106040938+BKnightHD@users.noreply.github.com>
2026-05-22 14:37:12 -0500

    Add files via upload

--- 01_Projects/01_Work/helper.sql
+++ 01_Projects/01_Work/helper.sql
@@ -1,21 +1,63 @@
--- Helper file
+-- ==============================================
+-- Script:      Helper File
+-- Description: Utility queries for inspecting and 
+--              modifying the transportation_report DB
+-- Author:      Brandon J. Knight
+-- ==============================================
 
-show columns from evergreen;
+USE transportation_report;
 
-use transportation_report;
-drop table msc, `one`, evergreen, wan_hai,sm_line;
+SELECT * FROM transportation_report.arbs;
+SELECT * FROM transportation_report.domestic;
+SELECT * FROM transportation_report.dray;
+SELECT * FROM transportation_report.evergreen;
+SELECT * FROM transportation_report.fowarders;
+SELECT * FROM transportation_report.msc;
+SELECT * FROM transportation_report.`one`;
+SELECT * FROM transportation_report.origin_chrgs;
+SELECT * FROM transportation_report.sm_line;
+SELECT * FROM transportation_report.wan_hai;
 
-select * from evergreen;
+-- ----------------------------------------------
+-- Inspect Table Structure
+-- ----------------------------------------------
+SHOW COLUMNS FROM evergreen;
 
--- change column data type
-alter table evergreen 
-modify column effective_date int,
-modify column end_date int;
+-- ----------------------------------------------
+-- Preview Table Data
+-- ----------------------------------------------
+SELECT * FROM evergreen;
 
-show columns from evergreen;
+-- ----------------------------------------------
+-- Modify Date Columns to Correct Data Type
+-- ----------------------------------------------
+ALTER TABLE evergreen 
+    MODIFY COLUMN effective_date INT,
+    MODIFY COLUMN end_date       INT;
 
--- Invoking date format change on columns in evergreen table
+-- Confirm column changes
+SHOW COLUMNS FROM evergreen;
+
+-- ----------------------------------------------
+-- Convert Excel Serial Dates to DD-MM-YYYY
+-- ----------------------------------------------
 SELECT 
     DATE_FORMAT(DATE_ADD('1899-12-30', INTERVAL effective_date DAY), '%d-%m-%Y') AS effective_date,
-    DATE_FORMAT(DATE_ADD('1899-12-30', INTERVAL end_date DAY), '%d-%m-%Y') AS end_date
+    DATE_FORMAT(DATE_ADD('1899-12-30', INTERVAL end_date DAY),       '%d-%m-%Y') AS end_date
 FROM evergreen;
+
+-- ----------------------------------------------
+-- Drop All Tables
+-- ----------------------------------------------
+DROP TABLE IF EXISTS msc,
+                     `one`,
+                     evergreen,
+                     wan_hai,
+                     sm_line,
+                     forwarders,
+                     spot,
+                     origin_chrgs,
+                     arbs,
+                     domestic;
+                     
+                     
