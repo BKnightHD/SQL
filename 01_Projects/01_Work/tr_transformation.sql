8fd6008d2592bb5ff0248eeb7e72cea8c1209a36
Brandon J Knight <106040938+BKnightHD@users.noreply.github.com>
2026-05-26 16:02:33 -0500

    Add files via upload

--- 01_Projects/01_Work/tr_transformation.sql
+++ 01_Projects/01_Work/tr_transformation.sql
@@ -2,6 +2,13 @@
 
 USE transportation_report;
 
+-- setting query timeout to 1 hour
+SET GLOBAL net_read_timeout = 360000;
+SET GLOBAL net_write_timeout = 360000;
+SET GLOBAL wait_timeout = 360000;
+SET GLOBAL interactive_timeout = 360000;
+
+
 -- Append all carrier tables into new table
 CREATE TABLE transportation_report AS
 SELECT * FROM evergreen
@@ -21,9 +28,11 @@ ALTER TABLE transportation_report
     ADD COLUMN arb_40ft DECIMAL(7,2) AFTER ocean_freight_base_charge_40ft,
     ADD COLUMN arb_40hc DECIMAL(7,2) AFTER ocean_freight_base_charge_40hc;
 
+
 -- Populate them by matching on origin columns
+SET SQL_SAFE_UPDATES = 0;
 UPDATE transportation_report tr
-LEFT JOIN arbs a
+INNER JOIN arbs a
     ON  tr.origin_continent = a.origin_continent
     AND tr.origin_country   = a.origin_country
     AND tr.origin_port      = a.origin_port
@@ -31,4 +40,4 @@ SET
     tr.arb_20ft = a.arb_20ft,
     tr.arb_40ft = a.arb_40ft,
     tr.arb_40hc = a.arb_40hc;
-
+SET SQL_SAFE_UPDATES = 1;
